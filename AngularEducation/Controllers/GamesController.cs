9afded6887340ddee1275591dfcd8594a5205ca5
Rallan123 <rasmus.jonasson91@gmail.com>
2015-09-29 21:07:52 +0200

    Initial api

---
+++ AngularEducation/Controllers/GamesController.cs
@@ -0,0 +1,28 @@
+﻿using AngularEducation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using AngularEducationBackend;
+
+namespace AngularEducation.Controllers
+{
+    public class GamesController : AngularEducationController
+    {
+        [HttpGet]
+        public IEnumerable<GameModel> GetGames()
+        {
+            return AngularEducationUnitOfWorkProvider.UnitOfWork.ReadStatement(p => p.GamePlayerService.ReadQuery(p.GamePlayerService.GetAll()).Select(x => new GameModel(x)));
+        }
+
+        public void Insert(GameModel gameModel)
+        {
+            AngularEducationUnitOfWorkProvider.UnitOfWork.ExcecuteStatement(p => p.GamePlayerService.Insert(new tblGame()
+            {
+                Name = gameModel.Name
+            }));
+        }
+    }
+}
---
+++ AngularEducation/Models/GameModel.cs
@@ -0,0 +1,19 @@
+﻿using AngularEducationBackend;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AngularEducation.Models
+{
+    [Serializable]
+    public class GameModel
+    {
+        public string Name { get; set; }
+        public GameModel(tblGame game)
+        {
+            Name = game.Name;
+        }
+    }
+}
