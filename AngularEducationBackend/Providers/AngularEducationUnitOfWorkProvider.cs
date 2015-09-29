24d591c7bc7714d19f15b6ad06f6a57a7f6a09da
Rallan123 <rasmus.jonasson91@gmail.com>
2015-09-29 21:09:11 +0200

    Fixed backend problem

---
+++ AngularEducationBackend/Providers/AngularEducationUnitOfWorkProvider.cs
@@ -0,0 +1,13 @@
+﻿using BackendGeneral.Providers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AngularEducationBackend.Providers
+{
+    public class AngularEducationUnitOfWorkProvider : UnitOfWorkProvider<AngularEducationUnitOfWork, AngularEducationDbContext, AngularEducationServiceProvider>
+    {
+    }
+}
